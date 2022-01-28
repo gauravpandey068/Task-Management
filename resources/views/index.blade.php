a591d47b84f661468e7e8eaa90d8a5b7ad179e0c
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-01-28 09:40:14 +0545

    task crud

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,13 @@
 @endsection
 
 @section('content')
-<div class="card">
+<div class="card mt-5 text-center text-primary fs-1 fw-bold">
     <div class="card-body">
         Task Management
     </div>
+    <div class="card-body">
+        Manage your tasks here
+    </div>
+
 </div>
 @endsection
